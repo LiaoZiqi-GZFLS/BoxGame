37a57c4762fd849af28327fb7ae9d921a7426ff2
LiaoZiqi-GZFLS <503546675@qq.com>
2024-12-06 11:38:58 +0800

    initial push

--- src/main/java/com/example/boxgame/Main.java
+++ src/main/java/com/example/boxgame/Main.java
@@ -3,6 +3,8 @@ package com.example.boxgame;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -21,6 +23,23 @@ public class Main extends Application {
         stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("img/1.png"))));
         stage.setResizable(false);
         stage.setTitle("awa");
+        // 设置关闭事件处理
+        stage.setOnCloseRequest(event -> {
+            // 创建一个确认对话框
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "您确定要退出程序吗？");
+            alert.setTitle("退出确认");
+            alert.setHeaderText(null);
+            alert.showAndWait().ifPresent(response -> {
+                if (response == ButtonType.OK) {
+                    // 用户选择“是”时，关闭窗口
+                    System.exit(0);
+                    return;
+                } else {
+                    // 用户选择“否”时，不执行任何操作，窗口保持打开状态
+                    event.consume();
+                }
+            });
+        });
         stage.setScene(scene);
         stage.show();
     }
