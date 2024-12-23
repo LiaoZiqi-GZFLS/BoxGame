a048145d259b3e8132c417a0f63d963ad75b00b8
LiaoZiqi-GZFLS <503546675@qq.com>
2024-12-23 11:30:18 +0800

    build

--- src/main/java/com/example/boxgame/Launcher.java
+++ src/main/java/com/example/boxgame/Launcher.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 public class Launcher {
     public static void main(String[] args) throws IOException {
         Test.main(args);
+        //HelloApplication.main(args);
+        //BoxGameApplication.main(args);
         Main.main(args);
     }
 }
